71e19943d3fae6882a91a4e76380e588be70d929
Cassandra Dixon <cassandra@cassandra-laptop.(none)>
2013-09-28 21:23:27 -0700

    catch data

---
+++ views.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, render_template, redirect
+import urllib
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+	query = "SELECT page_id, name, location FROM page WHERE page_id IN\
+			(SELECT page_id FROM location_post WHERE author_uid IN\
+			(SELECT uid2 FROM friend WHERE uid1=me()))"
+
+	print query
+
+	query = urllib.quote(query)
+	print(query) 
+
+	url = "https://graph.facebook.com/fql?q=" + query
+	data = urllib.urlopen(url).read()
+	print(data)
+	
+ 
+	return render_template('index.html',
+							data= data)
+
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
