92b74873c5c83f69848f13c08e6a3d1a1c955c26
Cassandra Dixon <cassandra@cassandra-laptop.(none)>
2013-09-29 08:49:43 -0700

    testing friend pull

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, session, request, render_template
-from flask_oauth import OAuth
+# from flask_oauth import OAuth
 import facebook
 
 
@@ -12,48 +12,55 @@ FACEBOOK_APP_SECRET = '	91e4d3b34430f67d65ad7ee76af65d28'
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
-oauth = OAuth()
+# oauth = OAuth()
 
-facebook = oauth.remote_app('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email'}
-)
+# facebook = oauth.remote_app('facebook',
+#     base_url='https://graph.facebook.com/',
+#     request_token_url=None,
+#     access_token_url='/oauth/access_token',
+#     authorize_url='https://www.facebook.com/dialog/oauth',
+#     consumer_key=FACEBOOK_APP_ID,
+#     consumer_secret=FACEBOOK_APP_SECRET,
+#     request_token_params={'scope': 'email'}
+# )
 
-@facebook.tokengetter
-def get_facebook_token():
-	return session.get('oauth_token')
+# @facebook.tokengetter
+# def get_facebook_token():
+# 	return session.get('oauth_token')
 
 @app.route('/')
 def index():
+    user = facebook.get_user_from_cookie(request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
+    if user:
+        graph = facebook.GraphAPI(user["access_token"])
+        profile = graph.get_object("me")
+        friends = graph.get_connections("me", "friends")
+    else:
+        friends = None
+    return render_template('index.html',
+                            friends= friends)
 
-    return render_template('index.html')
+
+# @app.route('/login')
+# def login():
+#     return fb.authorize(callback=url_for('facebook_authorized',
+#         next=request.args.get('index') or request.referrer or None,
+#         _external=True))
+#     # redirect("https://www.facebook.com/dialog/oauth?client_id=%s&redirect_url)
 
 
-@app.route('/login')
-def login():
-    return fb.authorize(callback=url_for('facebook_authorized',
-        next=request.args.get('index') or request.referrer or None,
-        _external=True))
-    # redirect("https://www.facebook.com/dialog/oauth?client_id=%s&redirect_url)
-
-
-@app.route('/login/authorized')
-@facebook.authorized_handler
-def facebook_authorized(resp):
-    if resp is None:
-        return 'Access denied: reason=%s error=%s' % (
-            request.args['error_reason'],
-            request.args['error_description']
-        )
-    session['oauth_token'] = (resp['access_token'], '')
-    me = facebook.get('/me')
-    return 'Logged in as id=%s name=%s redirect=%s' % \
-        (me.data['id'], me.data['name'], request.args.get('next'))
+# @app.route('/login/authorized')
+# @facebook.authorized_handler
+# def facebook_authorized(resp):
+#     if resp is None:
+#         return 'Access denied: reason=%s error=%s' % (
+#             request.args['error_reason'],
+#             request.args['error_description']
+#         )
+#     session['oauth_token'] = (resp['access_token'], '')
+#     me = facebook.get('/me')
+#     return 'Logged in as id=%s name=%s redirect=%s' % \
+#         (me.data['id'], me.data['name'], request.args.get('next'))
 
 
 if __name__ == '__main__':
