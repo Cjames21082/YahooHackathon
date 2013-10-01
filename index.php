afde7464a20267c33dc647b2f1673bfce1f4d47f
Cassandra Dixon <cassandra@cassandra-laptop.(none)>
2013-09-30 21:16:17 -0700

    add php code

---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.3.0/pure-min.css">
+	</head>
+	
+	<body>
+
+	
+		<div id="fb-root"></div>
+			<script>
+			  window.fbAsyncInit = function() {
+			  FB.init({
+			    appId      : '657737804244482', // App ID
+			    channelUrl : '//lit-harbor-9828.herokuapp.com/channel.html', // Channel File
+			    status     : true, // check login status
+			    cookie     : true, // enable cookies to allow the server to access the session
+			    xfbml      : true  // parse XFBML
+			  });
+
+			 
+			  FB.Event.subscribe('auth.authResponseChange', function(response) {
+			    if (response.status === 'connected') {
+			      testAPI();
+			    } else if (response.status === 'not_authorized') {
+			      FB.login();
+			    } else {
+			      FB.login();
+			    }
+			  });
+			  };
+
+			  // Load the SDK asynchronously
+			  (function(d){
+			   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+			   if (d.getElementById(id)) {return;}
+			   js = d.createElement('script'); js.id = id; js.async = true;
+			   js.src = "//connect.facebook.net/en_US/all.js";
+			   ref.parentNode.insertBefore(js, ref);
+			  }(document));
+
+			  // Here we run a very simple test of the Graph API after login is successful. 
+			  // This testAPI() function is only called in those cases. 
+			  function testAPI() {
+			    console.log('Welcome!  Fetching your information.... ');
+			    FB.api('/me', function(response) {
+			      console.log('Good to see you, ' + response.name + '.');
+			    });
+
+
+
+			    function showFriends(){
+
+			    };
+			  };
+
+			</script>
+
+	<!-- Login -->
+	<h1> Friends in Far Places </h1>
+	<p> When traveling let friends be your guide </p>
+
+	<?php
+
+			/* ASSIGN MY FACEBOOK  */
+			/* Note: this will not work unless you get your own access token from: https://developers.facebook.com/tools/explorer/    */
+			$graph_url = file_get_contents('https://graph.facebook.com/4202803?fields=photos&access_token=CAAJWNYQtZAgIBAIlrIZCZB4xwRrZAhLt97lnNnOAG6S61yzWNIB2i8dujzncZCidDcqxiv59tYm5GQ7dWjEVUB8a8zYFjTXZCciWRqrAdBlZCsSJqcmi5kSceTTt4M6ONrRh17W9AvznhZAbIjeQ1BwE1tpZAb1ZCES9lNCsndaDNEPYhx1DJjCgs0qFLsvqAaePl3fYqgjp6RsAZDZD');
+			
+			$graph_output = json_decode($graph_url);
+
+			/* TESTING TO SEE THE DATA */
+			// echo "<pre>";
+			// print_r($graph_output);
+			// echo "</pre>";
+			
+			/* PRINT OUT RESULTS */
+			for($i=0;$i<10;$i++) {
+				echo "<img src=\"" . $graph_output->photos->data[$i]->picture . "\"><br>";
+			}
+		
+			
+			?>
+	<fb:login-button autologoutlink="true" width="400" max-rows="1"></fb:login-button></a>
+	
+	
+
+	
+
+	</body>
+</html>
--- views.py
+++ views.py
@@ -16,68 +16,11 @@ FACEBOOK_APP_SECRET = '152696f9db120ef7d0bf878e7f73a5b0'
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
-# oauth = OAuth()
-
-# facebook = oauth.remote_app('facebook',
-#     base_url='https://graph.facebook.com/',
-#     request_token_url=None,
-#     access_token_url='/oauth/access_token',
-#     authorize_url='https://www.facebook.com/dialog/oauth',
-#     consumer_key=FACEBOOK_APP_ID,
-#     consumer_secret=FACEBOOK_APP_SECRET,
-#     request_token_params={'scope': 'email'}
-# )
-
-# @facebook.tokengetter
-# def get_facebook_token():
-# 	return session.get('oauth_token')
 
 @app.route('/')
 def index():
 
-    cookie = request.cookies.get("fbsr_" + FACEBOOK_APP_ID)
-    friends = None
-    if cookie:
-        encoded_sig, payload = map(str, cookie.split('.', 1))
-        sig = base64.urlsafe_b64decode(encoded_sig + "=" * ((4 - len(encoded_sig) % 4) % 4))
-        data = base64.urlsafe_b64decode(payload + "=" * ((4 - len(payload) % 4) % 4))
-
-        data = json.loads(data)
-        user = facebook.get_access_token_from_code(data["code"], "", FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
-    # user = facebook.get_user_from_cookie(request.cookies, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
-        if user:
-            graph = facebook.GraphAPI(user["access_token"])
-            profile = graph.get_object("me")
-            friends = graph.get_connections("me", "friends")
-
-    return render_template('index.html',
-                            friends= friends)
-
-
-
-
-
-# @app.route('/login')
-# def login():
-#     return fb.authorize(callback=url_for('facebook_authorized',
-#         next=request.args.get('index') or request.referrer or None,
-#         _external=True))
-#     # redirect("https://www.facebook.com/dialog/oauth?client_id=%s&redirect_url)
-
-
-# @app.route('/login/authorized')
-# @facebook.authorized_handler
-# def facebook_authorized(resp):
-#     if resp is None:
-#         return 'Access denied: reason=%s error=%s' % (
-#             request.args['error_reason'],
-#             request.args['error_description']
-#         )
-#     session['oauth_token'] = (resp['access_token'], '')
-#     me = facebook.get('/me')
-#     return 'Logged in as id=%s name=%s redirect=%s' % \
-#         (me.data['id'], me.data['name'], request.args.get('next'))
-
+   
 
 if __name__ == '__main__':
     app.run()
