2db66edc538cb76c3da140caa68c1601c4c92c1e
Cassandra Dixon <cassandra@cassandra-laptop.(none)>
2013-09-30 21:26:10 -0700

    testing fb graph api

--- views.py
+++ views.py
@@ -1,26 +1,26 @@
-from flask import Flask, redirect, url_for, session, request, render_template
-# from flask_oauth import OAuth
-import facebook
-import base64
-import urllib
-import urllib2
-import json
+# from flask import Flask, redirect, url_for, session, request, render_template
+# # from flask_oauth import OAuth
+# import facebook
+# import base64
+# import urllib
+# import urllib2
+# import json
 
 
-SECRET_KEY = 'development key'
-DEBUG = True
-FACEBOOK_APP_ID = '657737804244482'
-FACEBOOK_APP_SECRET = '152696f9db120ef7d0bf878e7f73a5b0'
+# SECRET_KEY = 'development key'
+# DEBUG = True
+# FACEBOOK_APP_ID = '657737804244482'
+# FACEBOOK_APP_SECRET = '152696f9db120ef7d0bf878e7f73a5b0'
 
 
-app = Flask(__name__)
-app.debug = DEBUG
-app.secret_key = SECRET_KEY
+# app = Flask(__name__)
+# app.debug = DEBUG
+# app.secret_key = SECRET_KEY
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
    
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
